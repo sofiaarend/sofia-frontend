2198f19286458052690c97be936ed5dba0066b65
Sofia <sofiaarend21@gmail.com>
2025-11-29 10:11:38 -0300

    add logo and start header

---
+++ src/app/cv/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-bg flex min-h-screen justify-center">
+      <main className="my-2 flex flex-col justify-center">
+        <h1 className="text-primary mt-10 text-6xl">Curriculo</h1>
+      </main>
+    </div>
+  );
+}
