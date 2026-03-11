22449b2bfa0956c7a306c3348e48b2d2a4aabe86
Sofia Arend <sofiaarend21@gmail.com>
2026-03-11 20:00:55 -0300

    adjust home layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,14 +5,11 @@ export default async function Home() {
   const t = await getTranslations();
 
   return (
-    <div className="bg-bg flex min-h-screen justify-center">
-      <main className="my-2 flex flex-col justify-center">
-        <h1 className="text-primary mt-10 text-6xl">{t('title')}</h1>
-        <div className="flex h-full items-center justify-center">
-          <Link
-            href="/resume"
-            className="text-primary w-full rounded-md border px-2 py-3 text-center"
-          >
+    <div className="bg-bg flex min-h-screen">
+      <main className="mx-4 my-2 flex w-full flex-col">
+        <h1 className="text-fg mt-16 ml-4 text-8xl">{t('title')}</h1>
+        <div className="flex h-full w-full items-center justify-center">
+          <Link href="/resume" className="text-primary rounded-md border px-2 py-3 text-center">
             {t('resume.title')}
           </Link>
         </div>
